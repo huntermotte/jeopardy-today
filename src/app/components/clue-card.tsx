06d7b7fbfac0062100b8573f39db69780285c1d6
huntermotte <hunter@harvestedfinancial.com>
2024-07-08 23:07:00 -0400

    use jeopardy blue

--- src/app/components/clue-card.tsx
+++ src/app/components/clue-card.tsx
@@ -1,6 +1,6 @@
 export default function ClueCard() {
     return (
-        <div className="clue-container">
+        <div className="clue-container w-full text-center border-2 bg-jeopardy-blue">
             <h1>This quarterback was famous for the Dab</h1>
         </div>
     )
