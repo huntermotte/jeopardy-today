937e569fa503fcc5571da05be6e9705943ab996f
huntermotte <hunter@harvestedfinancial.com>
2024-08-13 09:49:42 -0400

    cleanup, add nextui

--- src/app/clue-card.tsx
+++ src/app/clue-card.tsx
@@ -1,43 +1,53 @@
 "use client";
 
 import {useEffect, useState} from "react";
+import {Skeleton} from "@nextui-org/skeleton";
 
 export default function ClueCard() {
     const [jeopardyQuestion, setJeopardyQuestion] = useState(null);
+    const [showAnswer, setShowAnswer] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchRandomRow = async () => {
-            setLoading(true);
-            setError(null);
-
-            try {
-                const response = await fetch('/api/read-local-csv');
-
-                if (!response.ok) {
-                    throw new Error('Failed to fetch random row');
-                }
-
-                const result = await response.json();
-                if (result.randomRow) {
-                    setJeopardyQuestion(result.randomRow);
-                }
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         fetchRandomRow();
     }, []);
 
+    const fetchRandomRow = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch('/api/read-local-csv');
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch random row');
+            }
+
+            const result = await response.json();
+            if (result.randomRow) {
+                setJeopardyQuestion(result.randomRow);
+            }
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const revealAnswer = () => {
+        setShowAnswer(true);
+    }
+
     return (
         <div className="clue-container w-full text-center border-2 bg-jeopardy-blue p-6 py-20 font-OPTIKorinnaAgency uppercase tracking-wider cursor-pointer
-        lg:p-10 lg:min-h-80 lg:flex lg:justify-center lg:items-center lg:text-4xl"
-            >
-            {jeopardyQuestion && <h1 className="text-white lg:w-3/4">{jeopardyQuestion.Question}</h1>}
+        lg:p-10 lg:min-h-80 lg:flex lg:justify-center lg:items-center lg:text-4xl lg:w-screen"
+            onClick={revealAnswer}>
+            {jeopardyQuestion ? (
+                <h1 className="text-white lg:w-3/4">{!showAnswer ? jeopardyQuestion.Question : jeopardyQuestion.Answer}</h1>
+            ) : (
+                <h1 className="text-white lg:w-3/4">Loading a good question...</h1>
+            )}
         </div>
     )
 }
